844b9b9f2fb871e5fce9d10de8930fd0b45d3e83
Pierre Malarme <pmalarme@microsoft.com>
2022-10-24 18:55:19 +0200

    Update Dapr SDK version, add assignment 7 to invoke a service from a service using Dapr, add assignment 8 to use Redis as a state store, update state store to use the DaprClient instead of the RestTemplate

--- TrafficControlService/src/main/java/dapr/traffic/vehicle/DaprVehicleStateRepository.java
+++ TrafficControlService/src/main/java/dapr/traffic/vehicle/DaprVehicleStateRepository.java
@@ -1,56 +1,30 @@
 package dapr.traffic.vehicle;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import io.dapr.client.DaprClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
 public class DaprVehicleStateRepository implements VehicleStateRepository {
 	
     private static final String DAPR_STORE_NAME = "statestore";
-    
-    private static class DaprStateEntry {
-        private final String key;
-        private final VehicleState value;
 
-        public DaprStateEntry(final String key, final VehicleState value) {
-            this.key = key;
-            this.value = value;
-        }
+    private final DaprClient daprClient;
 
-        public String getKey() {
-            return this.key;
-        }
-
-        public VehicleState getValue() {
-            return this.value;
-        }
-    }
-
-    private final RestTemplate restTemplate;
-
-    public DaprVehicleStateRepository(final RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public DaprVehicleStateRepository(final DaprClient daprClient) {
+        this.daprClient = daprClient;
     }
 
     @Override
     public VehicleState saveVehicleState(VehicleState vehicleState) {
-        var entries = new DaprStateEntry[] {
-                new DaprStateEntry(vehicleState.licenseNumber(), vehicleState)
-        };
-
-        var daprUrl = System.getProperty("DAPR_BASE_URL", "http://localhost:3600/v1.0/state/");
-        restTemplate.postForEntity(daprUrl + DAPR_STORE_NAME, entries, Object.class);
-
-        return vehicleState;
+        daprClient.saveState(DAPR_STORE_NAME, vehicleState.licenseNumber(), vehicleState).block();
+        Mono<io.dapr.client.domain.State<VehicleState>> result = daprClient.getState(DAPR_STORE_NAME, vehicleState.licenseNumber(), VehicleState.class);
+        return result.block().getValue();
     }
 
     @Override
     public Optional<VehicleState> getVehicleState(String licenseNumber) {
-    	var daprUrl = System.getProperty("DAPR_BASE_URL", "http://localhost:3600/v1.0/state/");
-        var state = restTemplate.getForObject(daprUrl + DAPR_STORE_NAME + "/" + licenseNumber, VehicleState.class);
-
-        return Optional.ofNullable(state);
+    	Mono<io.dapr.client.domain.State<VehicleState>> result = daprClient.getState(DAPR_STORE_NAME, licenseNumber, VehicleState.class);
+        return Optional.ofNullable(result.block().getValue());
     }
 }
