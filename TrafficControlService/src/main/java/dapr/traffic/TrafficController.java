daea3e5e9274d64c03c2946b1300403c7923e378
Pierre Malarme <pmalarme@microsoft.com>
2023-02-13 19:59:25 +0100

    Add health endpoint for health probe

--- TrafficControlService/src/main/java/dapr/traffic/TrafficController.java
+++ TrafficControlService/src/main/java/dapr/traffic/TrafficController.java
@@ -9,6 +9,7 @@ import dapr.traffic.violation.SpeedingViolationCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,11 @@ public class TrafficController {
         this.fineCollectionClient = fineCollectionClient;
         this.speedingViolationCalculator = speedingViolationCalculator;
         this.vehicleStateRepository = vehicleStateRepository;
+    }
+
+    @GetMapping(path = "/heatlh")
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping(path = "/entrycam")
