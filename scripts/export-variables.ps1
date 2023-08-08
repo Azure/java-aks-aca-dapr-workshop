bb3ce5c15caf8fd335b2bbd16b556da9c56d4f2c
Pierre Malarme <pmalarme@microsoft.com>
2023-08-08 20:37:11 +0200

    Add powershell export script

---
+++ scripts/export-variables.ps1
@@ -0,0 +1,97 @@
+# ---------------------------------------------------------------------------- #
+#                                   FUNCTIONS                                  #
+# ---------------------------------------------------------------------------- #
+
+function Write-New-File {
+  Write-Output "# Set variables script" | Out-File -FilePath .\set-vars.ps1
+}
+
+function Write-To-File($Text) {
+  Write-Output $Text | Out-File -FilePath .\set-vars.ps1 -Append
+}
+
+function Write-Blank-Line {
+  Write-To-File ""
+}
+
+function Write-Variable {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+    [Parameter(Mandatory=$false)]
+    [string]$Value
+  )
+  $ValueToSet = ""
+  if ($Value) {
+    $ValueToSet = "$Value"
+  }
+
+  Write-To-File "Set-Variable -Name `"$Name`" -Value `"$ValueToSet`" -Scope `"Global`""
+  Write-To-File "Write-Output `"$Name=`$$Name`""
+  Write-Blank-Line
+}
+
+function Write-Environment-Variable {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+    [Parameter(Mandatory=$false)]
+    [string]$Value
+  )
+  $ValueToSet = ""
+  if ($Value) {
+    $ValueToSet = "`"$Value`""
+  }
+
+  Write-To-File "`$env:$Name = $ValueToSet"
+  Write-To-File "Write-Output `"$Name=`$env:$Name`""
+  Write-Blank-Line
+}
+
+# ---------------------------------------------------------------------------- #
+#                                 FILE CREATION                                #
+# ---------------------------------------------------------------------------- #
+
+Write-New-File
+Write-Blank-Line
+
+# ---------------------------- Supporting Services --------------------------- #
+
+$SubscriptionId = az account show --query id -o tsv
+Write-Variable -Name "SUBSCRIPTION" -Value "$SubscriptionId"
+
+Write-Variable -Name "SERVICE_BUS" -Value "$SERVICE_BUS"
+if ($SERVICE_BUS) {
+  $ServiceBusConnectionString = az servicebus topic authorization-rule keys list --resource-group rg-dapr-workshop-java --namespace-name $SERVICE_BUS --topic-name test --name DaprWorkshopJavaAuthRule  --query primaryConnectionString --output tsv
+  Write-Variable -Name "SERVICE_BUS_CONNECTION_STRING" -Value "$ServiceBusConnectionString"
+} else {
+  Write-Variable -Name "SERVICE_BUS_CONNECTION_STRING"
+}
+
+Write-Variable -Name "REDIS" -Value "$REDIS"
+Write-Variable -Name "REDIS_HOSTNAME" -Value "$REDIS_HOSTNAME"
+Write-Variable -Name "REDIS_SSL_PORT" -Value "$REDIS_SSL_PORT"
+Write-Variable -Name "REDIS_PRIMARY_KEY" -Value "$REDIS_PRIMARY_KEY"
+
+Write-Variable -Name "LOG_ANALYTICS_WORKSPACE_CUSTOMER_ID" -Value "$LOG_ANALYTICS_WORKSPACE_CUSTOMER_ID"
+Write-Variable -Name "LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET" -Value "$LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET"
+
+Write-Variable -Name "INSTRUMENTATION_KEY" -Value "$INSTRUMENTATION_KEY"
+
+Write-Variable -Name "CONTAINER_REGISTRY" -Value "$CONTAINER_REGISTRY"
+Write-Variable -Name "CONTAINER_REGISTRY_URL" -Value "$CONTAINER_REGISTRY_URL"
+
+Write-Variable -Name "COSMOS_DB" -Value "$COSMOS_DB"
+
+Write-Variable -Name "APP_ID" -Value "$APP_ID"
+Write-Variable -Name "SERVICE_PRINCIPAL_ID" -Value "$SERVICE_PRINCIPAL_ID"
+
+Write-Variable -Name "KEY_VAULT" -Value "$KEY_VAULT"
+
+# ----------------------------------- Apps ----------------------------------- #
+
+Write-Variable -Name "VEHICLE_REGISTRATION_SERVICE_FQDN" -Value "$VEHICLE_REGISTRATION_SERVICE_FQDN"
+
+Write-Variable -Name "TRAFFIC_CONTROL_SERVICE_FQDN" -Value "$TRAFFIC_CONTROL_SERVICE_FQDN"
+
+Write-Environment-Variable -Name "TRAFFIC_CONTROL_SERVICE_BASE_URL" -Value "https://$TRAFFIC_CONTROL_SERVICE_FQDN"
